87e38a932eadae7add123f1160c39242d4f2adde
Ritik dubey <95172948+Hritik-22@users.noreply.github.com>
2025-06-12 20:45:56 +0530

    app.js
    console frontend url

--- Server/app.js
+++ Server/app.js
@@ -5,6 +5,7 @@ app.use(express.json());
 
 //  CORS setup - 
 const cors = require("cors");
+console.log(process.env.FRONTEND_URL)
 app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173", credentials: true }));
 
 
